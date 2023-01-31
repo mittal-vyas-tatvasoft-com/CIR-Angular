e862616141758c8e8b9826543193de2abb48d97d
Trusha <trusha.kalariya@tatvasoft.com>
2023-01-31 09:48:03 +0530

    initial commit

---
+++ src/app/core/auth/components/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  //TODO: add comment for every methods
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedMaterialModule } from './material/shared-material.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, SharedMaterialModule],
+  exports: [SharedMaterialModule],
+})
+export class SharedModule {}
