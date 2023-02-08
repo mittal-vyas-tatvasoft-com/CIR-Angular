5dbfb6f4ae1e8f9c87e82b417760e13a35edf0b4
Trusha <trusha.kalariya@tatvasoft.com>
2023-02-08 14:05:06 +0530

    added stories

---
+++ src/app/shared/modules/form-control/components/button/button.stories.ts
@@ -0,0 +1,47 @@
+// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
+import { CommonModule } from '@angular/common';
+import { moduleMetadata } from '@storybook/angular';
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { SharedMaterialModule } from 'src/app/shared/material/shared-material.module';
+import { ButtonComponent } from './button.component';
+
+// More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
+export default {
+  title: 'Button-Component',
+  component: ButtonComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule, SharedMaterialModule],
+    }),
+    // componentWrapperDecorator(
+    //   (story) =>
+    //     `<div class="storybook-ng-content-wrapper storybook-grey">${story}</div>`,
+    // ),
+  ],
+  // More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
+  argTypes: {
+    color: {
+      control: 'text',
+      description: "value should be  'primary' | 'accent' | 'warn'",
+    },
+  },
+} as Meta;
+
+// More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
+const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
+  props: args,
+});
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/angular/writing-stories/args
+Primary.args = {
+  label: 'Button',
+  color: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+  color: 'accent',
+  isDisabled: true,
+};
