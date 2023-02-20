a2a3b0f2790b052ba925c5c4e80c8e4e5e903e76
Shubham-Padiya <95012298+Shubham-Padiya@users.noreply.github.com>
2023-02-20 12:42:06 +0530

    login with API integration

---
+++ src/app/shared/modules/form-control/components/captcha/captcha.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-captcha',
+  templateUrl: './captcha.component.html',
+  styleUrls: ['./captcha.component.scss'],
+})
+export class CaptchaComponent {
+  SITE_KEY = environment.SITE_KEY;
+
+  @Input() captchaForm: FormGroup;
+  @Output() onResolveClick = new EventEmitter<string>();
+
+  resolved(captchaResponse: string) {
+    this.onResolveClick.emit(captchaResponse);
+  }
+}
