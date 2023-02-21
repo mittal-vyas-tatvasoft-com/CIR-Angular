8ac8964171b551ee331b04a6809a08dfeff6cc25
mittal.vyas <mittal.vyas@tatvasoft.com>
2023-02-21 18:51:14 +0530

    email changes

--- src/app/modules/global-configuration/email/components/email-list/email-list.component.ts
+++ src/app/modules/global-configuration/email/components/email-list/email-list.component.ts
@@ -1,8 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
+import { DynamicFieldTypes } from 'src/app/shared/common/enum';
+import { dynamicFields } from 'src/app/shared/common/interfaces/constants.static';
+import { ResponseModel } from 'src/app/shared/common/interfaces/response.interface';
+import { CommonFacadeService } from 'src/app/shared/services/common/common-facade.service';
+import { SnackbarService } from 'src/app/shared/snackbar/snackbar.service';
+import { emailControl } from '../../config/emails.config';
+import { Email } from '../../interface/emails.interface';
+import { EmailService } from '../../service/email.service';
 
 @Component({
   selector: 'app-email-list',
   templateUrl: './email-list.component.html',
   styleUrls: ['./email-list.component.scss'],
 })
-export class EmailListComponent {}
+export class EmailListComponent implements OnInit {
+  private ngUnsubscribe$ = new Subject<void>();
+  emailList: Email[];
+  isDisabled = true;
+  emailContentList: Email[] = [];
+  form: FormGroup;
+  emailConfig = emailControl;
+  dynamicFields = DynamicFieldTypes;
+  dynamicData = dynamicFields;
+  cultureId: number;
+  isDefaultData: boolean;
+
+  constructor(
+    private emailService: EmailService,
+    private fb: FormBuilder,
+    private snackbarService: SnackbarService,
+    public commonFacadeService: CommonFacadeService,
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      languageField: [1],
+    });
+    this.commonFacadeService.fetchCultureList().subscribe();
+    this.cultureId = this.form.get('languageField')?.value;
+    this.getEmails(this.cultureId);
+  }
+
+  getEmails(cultureId: number) {
+    this.emailService.getEmails(cultureId).subscribe({
+      next: (res: ResponseModel<Email[]>) => {
+        if (res.result) {
+          this.emailList = res.data;
+          this.emailContentList = res.data;
+          if (res.data[0].cultureId == 1) this.isDefaultData = true;
+        }
+      },
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  contentChange(event: any) {
+    console.log(event);
+    this.emailContentList[event.fieldTypeId - 1].content = event.content.data;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  subjectChange(event: any) {
+    this.emailContentList[event.fieldTypeId - 1].subject = event.content;
+  }
+
+  onSubmit() {
+    this.emailContentList.map((val) => (val.cultureId = this.cultureId));
+    this.emailService
+      .saveEmails(this.emailContentList)
+      .pipe(takeUntil(this.ngUnsubscribe$))
+      .subscribe((res: ResponseModel<Email[]>) => {
+        if (res.result) {
+          this.snackbarService.success(res.message);
+        } else {
+          this.snackbarService.error(res.message);
+        }
+      });
+  }
+
+  onLanguageSelect(cultureId: number) {
+    this.cultureId = cultureId;
+    this.getEmails(cultureId);
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
+  }
+}
---
+++ src/app/modules/global-configuration/email/service/email.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CultureModel } from 'src/app/shared/common/interfaces/culture.interface';
+import { ResponseModel } from 'src/app/shared/common/interfaces/response.interface';
+import { environment } from 'src/environments/environment';
+import { Email } from '../interface/emails.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmailService {
+  constructor(private http: HttpClient) {}
+
+  getEmails(cultureId: number): Observable<ResponseModel<Email[]>> {
+    return this.http.get<ResponseModel<Email[]>>(
+      environment.baseURL + 'GlobalConfigurationEmails/' + cultureId,
+    );
+  }
+
+  saveEmails(emails: Email[]) {
+    return this.http.post<ResponseModel<Email[]>>(
+      environment.baseURL + 'GlobalConfigurationEmails/create',
+      emails,
+    );
+  }
+
+  getCultureList(): Observable<ResponseModel<CultureModel[]>> {
+    return this.http.get<ResponseModel<CultureModel[]>>(
+      `${environment.baseURL}Common/GetCultures`,
+    );
+  }
+}
