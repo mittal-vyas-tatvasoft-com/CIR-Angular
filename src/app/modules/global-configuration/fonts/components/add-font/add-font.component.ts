2372c4b2a225a39259ff8db8c531f866b7ec234f
Shubham-Padiya <95012298+Shubham-Padiya@users.noreply.github.com>
2023-02-23 11:55:49 +0530

    add-font structure and fixes

---
+++ src/app/modules/global-configuration/fonts/components/add-font/add-font.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-font',
+  templateUrl: './add-font.component.html',
+  styleUrls: ['./add-font.component.scss']
+})
+export class AddFontComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,17 +4,11 @@ import { SharedMaterialModule } from './material/shared-material.module';
 import { EditorComponent } from './component/editor/editor.component';
 import { FormsModule } from '@angular/forms';
 import { CKEditorModule } from 'ckeditor4-angular';
-import { DeleteConfirmationDialogComponent } from './components/dialogs/delete-confirmation-dialog/delete-confirmation-dialog/delete-confirmation-dialog.component';
-import { NoDataFoundComponent } from './components/no-data-found/no-data-found/no-data-found.component';
 import { SnackbarService } from './snackbar/snackbar.service';
 @NgModule({
-  declarations: [
-    EditorComponent,
-    DeleteConfirmationDialogComponent,
-    NoDataFoundComponent,
-  ],
+  declarations: [EditorComponent],
   imports: [CommonModule, SharedMaterialModule, FormsModule, CKEditorModule],
-  exports: [SharedMaterialModule, EditorComponent, NoDataFoundComponent],
+  exports: [SharedMaterialModule, EditorComponent],
   providers: [SnackbarService],
 })
 export class SharedModule {}
