fdb971a11c5b3873cf3161b535a1692f1b0db4a1
RajviKotecha <rajvi.kotecha@tatvasoft.com>
2023-02-27 15:52:21 +0530

    Holidays blank structure

---
+++ src/app/modules/global-configuration/holidays/components/add-edit-holiday/add-edit-holiday.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-holiday',
+  templateUrl: './add-edit-holiday.component.html',
+  styleUrls: ['./add-edit-holiday.component.scss'],
+})
+export class AddEditHolidayComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
