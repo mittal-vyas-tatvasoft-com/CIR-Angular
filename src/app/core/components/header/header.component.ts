41e5b0daa5b743bd4f986a354e3a00c404b560c3
Shubham-Padiya <95012298+Shubham-Padiya@users.noreply.github.com>
2023-03-01 10:31:27 +0530

    logout functionality

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { LoginService } from '../../auth/services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,13 @@ export class HeaderComponent {
   @Input() isHandset: boolean | null;
   @Output() onMenuIconClick = new EventEmitter();
 
+  constructor(public loginService: LoginService) {}
   onClick() {
     this.onMenuIconClick.emit();
+  }
+
+  //clear the token and logout the user
+  logout() {
+    this.loginService.logout();
   }
 }
