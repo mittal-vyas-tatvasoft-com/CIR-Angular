1dad3764ca6bac78fbcd2586927ada3f6cc0f7ec
Divyapalsinh Vala <divyapalsinh@aramex.com>
2023-03-01 12:41:21 +0530

    Production settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURL: 'https://web2.anasource.com/CIRBackend/api/',
+  SITE_KEY: '6LcIiBIiAAAAAJdVw_WfOIax4hvXV6VrPxFLAQvg',
 };
